25f24ac66d230241270868d47b02516df50d8573
Your Name <julianwoo@me.com>
2023-11-27 14:45:41 -0500

    fixing bugs, fixing import statements. We had an issue where we were using the incorrect import statements

--- src/main/java/interface_adapter/login/LoginPresenter.java
+++ src/main/java/interface_adapter/login/LoginPresenter.java
@@ -1,10 +1,10 @@
 package fantastticfour.src.main.java.interface_adapter.login;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_in.LoggedInState;
-import interface_adapter.logged_in.LoggedInViewModel;
-import use_case.login.LoginOutputBoundary;
-import use_case.login.LoginOutputData;
+import fantastticfour.src.main.java.interface_adapter.logged_in.LoggedInState;
+import fantastticfour.src.main.java.interface_adapter.logged_in.LoggedInViewModel;
+import fantastticfour.src.main.java.use_case.login.LoginOutputBoundary;
+import fantastticfour.src.main.java.use_case.login.LoginOutputData;
 
 public class LoginPresenter implements LoginOutputBoundary {
 
