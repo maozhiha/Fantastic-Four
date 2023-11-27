eaec2e67f73563dc58f383f854d502fa12214668
Your Name <julianwoo@me.com>
2023-11-27 14:54:02 -0500

    fixing bugs, fixing import statements. We had an issue where we were using the incorrect import statements

--- src/main/java/use_case/welcome_user/WelcomeUserInteractor.java
+++ src/main/java/use_case/welcome_user/WelcomeUserInteractor.java
@@ -1,7 +1,7 @@
 package fantastticfour.src.main.java.use_case.welcome_user;
 
 import use_case.welcome_user.WelcomeUserInputBoundary;
-import use_case.welcome_user.WelcomeUserOutputBoundary;
+import use_case.welcome_user.WelcomeuserOutputBoundary;
 
 
 public class WelcomeUserInteractor implements WelcomeUserInputBoundary {
