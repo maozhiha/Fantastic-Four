99611fa483577edf39adf6a5b6c709753878a0b4
Your Name <julianwoo@me.com>
2023-11-27 14:51:30 -0500

    fixing bugs, fixing import statements. We had an issue where we were using the incorrect import statements

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -1,6 +1,6 @@
 package fantastticfour.src.main.java.use_case.login;
 
-import entity.User;
+import fantastticfour.src.main.java.entity.User;
 
 public interface LoginUserDataAccessInterface {
     boolean existsByName(String identifier);
